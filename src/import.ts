500c792758baaffc88231306c61318326c975ac7
FAISAL YUSUF <faysalyusuf03@gmail.com>
2025-04-27 16:27:47 +0100

    Modules in ts

---
+++ src/import.ts
@@ -0,0 +1,2 @@
+import {areaOfRectangle} from "./modules"
+console.log(areaOfRectangle(20, 50))
---
+++ src/modules.ts
@@ -0,0 +1,5 @@
+// ASSIGNMENT
+// Create a module to export a function that calculates the area of a rectangle.
+export function areaOfRectangle(length: number, breadth: number):number {
+    return length * breadth
+}
